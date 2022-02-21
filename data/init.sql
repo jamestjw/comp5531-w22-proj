558b6d9a60918b6b3c730a7afbef8d4aab7534d3
James Tan <james_tan97@outlook.com>
2022-02-21 17:00:02 -0500

    Add data/init.sql file

---
+++ data/init.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS dev;
+
+CREATE DATABASE IF NOT EXISTS dev;
+
+use dev;
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	is_admin BOOLEAN NOT NULL,
+	password_digest VARCHAR(60) NOT NULL,
+	student_id INT UNSIGNED,
+	created_at TIMESTAMP,
+	updated_at TIMESTAMP,
+	UNIQUE (email)
+);
+
+-- Insert admin user
+-- This is idempotent due to the unique constraint on the `email` column
+INSERT INTO users (first_name, last_name, email, is_admin, password_digest, created_at, updated_at)
+VALUES ('ADMIN', 'USER', 'admin@concordia.ca', 1, 'root', now(), now());
