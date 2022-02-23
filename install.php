18e970ddfc0c705f92c982bf11ed393ce0c92986
James Tan <james_tan97@outlook.com>
2022-02-23 11:32:17 -0500

    Make database names dynamic based on the environment

--- install.php
+++ install.php
@@ -10,7 +10,9 @@ require "config.php";
 
 try {
     $connection = new PDO("mysql:host=$host", $username, $password, $options);
+    $database_name = $environment."_cga";
     $sql = file_get_contents("data/init.sql");
+    $sql = sprintf($sql, $database_name);
     $connection->exec($sql);
     
     echo "Database and table users created successfully.";
