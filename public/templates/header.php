4989162f3889882cefdaa5307d3f45046e699cd9
drouba <andreanne.chartrand@gmail.com>
2022-02-27 18:53:58 -0500

    add setup for bootstrap, add navbar menu in header

--- public/templates/header.php
+++ public/templates/header.php
@@ -6,12 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
-
     <title>COMP 5531 Database App</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
   </head>
 
   <body>
     <h1>COMP 5531 Database App</h1>
+    <nav class="navbar navbar-expand-sm navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">CGA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse me-auto" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link" href="#">Courses</a>
+            <a class="nav-link" href="#">Account Settings</a>
+            <a class="nav-link" href="#">Email</a>
+            <a class="nav-link" href="instructors_list.php">Instructors</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+    
+
     <?php
     require_once (dirname(__FILE__)."/../../common.php");
 
@@ -24,6 +43,11 @@
       echo "<p>You are not logged in. <a href=\"login.php\">Login</a></p>";
     }
     ?>
+
+
+
+    <script src="js/bootstrap.bundle.js"></script>
+    
 
   </body>
 </html>
