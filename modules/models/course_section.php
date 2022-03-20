8333aa2aaa277b74a8d2a08b268fc6f669208e31
Zachary-Concordia <91906050+Zachary-Concordia@users.noreply.github.com>
2022-03-20 09:13:15 -0400

    Added belongs_to associations to respective classes.

--- modules/models/course_section.php
+++ modules/models/course_section.php
@@ -4,6 +4,13 @@ class CourseSection extends Record {
 
     static protected $table_name = "course_sections";
 
+    static protected  $belongs_to = array(
+        "course_offering"=> array(
+            "class_name" => "CourseOffering",
+            "foreign_key" => "offering_id"
+            )
+        );
+
     public $id;
     public $offering_id;
     public $course_section_code;
