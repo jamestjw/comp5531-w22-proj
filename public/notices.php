1eb5c6020618e60ca74e870a45c02d37ffee946f
drouba <andreanne.chartrand@gmail.com>
2022-03-21 09:20:08 -0400

    change notice to announcement

--- public/notices.php
+++ public/notices.php
@@ -32,42 +32,23 @@ if ($notices && count($notices)) { ?>
 
         <?php foreach ($notices as $row) { ?>
 
-<<<<<<< HEAD
             <h3>Announcement #<?php echo escape($row->id); ?></h3>
-=======
-            <h3>Notice #<?php echo escape($row->id); ?></h3>
->>>>>>> f4e6c87204f67e516a4215fc15b7663e141c870c
             <h5>Posted on <?php echo escape($row->created_at);?></h5>
             <p><?php echo escape($row->notice)?></p>
 
         <?php } ?>
 
 <?php } else { ?>
-<<<<<<< HEAD
     <blockquote>No Announcements</blockquote>
 <?php }?>
 
-<?php if (get_current_role() == "admin"){ ?>
-<h2>New announcements</h2>
+<!-- ADD IF STATEMENT TO SHOW NEW NOTICE ONLY IF USER == ADMNIN -->
+<h2>New announcement</h2>
 
 <form method="post">
     <label for="notice_text">Announcement</label>
     <textarea name="notice_text" cols="40" rows="5"></textarea>
     <input type="submit" name="submit" value="Submit">
 </form>
-<?php }?>
-=======
-    <blockquote>No Notices</blockquote>
-<?php }?>
-
-<!-- ADD IF STATEMENT TO SHOW NEW NOTICE ONLY IF USER == ADMNIN -->
-<h2>New notice</h2>
-
-<form method="post">
-    <label for="notice_text">Notice</label>
-    <textarea name="notice_text" cols="40" rows="5"></textarea>
-    <input type="submit" name="submit" value="Submit">
-</form>
->>>>>>> f4e6c87204f67e516a4215fc15b7663e141c870c
 
 <?php include "templates/footer.php"; ?>
