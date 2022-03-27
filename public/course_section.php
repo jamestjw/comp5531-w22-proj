393ee25cfd82d77197f7d8a425764e2ea492ffe5
drouba <andreanne.chartrand@gmail.com>
2022-03-27 12:36:20 -0400

    add course_section page and link to course_section page from course list page

---
+++ public/course_section.php
@@ -0,0 +1,15 @@
+<?php require_once(dirname(__FILE__)."/../modules/ensure_logged_in.php"); ?>
+
+<?php
+
+require_once "../modules/models/user.php";
+require_once "../common.php";
+require_once "../modules/models/course_section.php";
+require_once "../modules/models/course.php";
+
+
+?>
+
+
+<?php include "templates/header.php"; ?>
+
