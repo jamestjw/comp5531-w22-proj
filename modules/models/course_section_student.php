871d12ebe54214023cafa7bd7d1535ab1865dcd0
drouba <andreanne.chartrand@gmail.com>
2022-03-27 17:10:20 -0400

    student list page now also shows the students in each section

--- modules/models/course_section_student.php
+++ modules/models/course_section_student.php
@@ -6,6 +6,17 @@ class CourseSectionStudent extends Record
 {
     protected static $table_name = "course_section_students";
 
+    protected static $belongs_to = array(
+        "user" => array(
+            "class_name" => "User",
+            "foreign_key" => "user_id",
+        ),
+        "course_section" => array(
+            "class_name" => "CourseSection",
+            "foreign_key" => "section_id",
+        )
+        );
+
     public $user_id;
     public $section_id;
     public $created_at;
