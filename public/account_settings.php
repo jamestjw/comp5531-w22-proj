d520254d347e38cbe68fda1a238799bb97b86b01
chris7neves <christopher.neves@hotmail.ca>
2022-04-02 17:18:26 -0400

    checking student id for only digits, fixed password change bug, student id can only be changed if already set

--- public/account_settings.php
+++ public/account_settings.php
@@ -18,7 +18,9 @@ input { display: table-cell; }
 li    { display: table-row;}
 </style>
 
-<form method="post" style="display:table">
+<?php // The action field of the below form redirects the form contents to the page itself, 
+      //and prevents users from injecting malicious php code?>
+<form method="post" style="display:table" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
  <ul>
   <li >
     <label for="first_name">First Name:  <?php echo escape($user->first_name)?></label>
@@ -28,10 +30,12 @@ li    { display: table-row;}
     <label for="last_name">Last Name:  <?php echo escape($user->last_name)?></label>
     <input type="text" id="last_name" name="last_name">
   </li>
+  <?php if (!is_null($user->student_id)) {?>
   <li>
     <label for="student_id">Student ID: <?php echo escape($user->student_id)?></label>
     <input type="text" id="student_id" name="student_id"></input>
   </li>
+  <?php } ?>
   <li>
     <label for="email">Email: <?php echo escape($user->email)?></label>
     <input type="email" id="email" name="email"></input>
@@ -60,13 +64,22 @@ if (isset($_POST['submit'])) {
 
   empty($_POST["first_name"]) ?: $user->first_name = $_POST["first_name"];
   empty($_POST["last_name"]) ?: $user->last_name = $_POST["last_name"];
-  empty($_POST["student_id"]) ?: $user->student_id = $_POST["student_id"];
   empty($_POST["email"]) ?: $user->email = $_POST["email"];
 
-  if (!empty($_POST["password"]) && ($_POST["password"] == $_POST["password_conf"])) {
-    $user->password_digest = password_hash($_POST["password"], PASSWORD_DEFAULT);
-  } else {
-    echo "Password and password confirmation does not match!";
+  if (!empty($_POST["student_id"])) {
+    if (ctype_digit($_POST["student_id"])) {
+      $user->student_id = $_POST["student_id"];
+    } else {
+      echo "The entered student ID - '".escape($_POST["student_id"]."' is not valid and should only contain digits.");
+    }
+  }
+
+  if (!empty($_POST["password"])) {
+    if ($_POST["password"] == $_POST["password_conf"]) {
+      $user->password_digest = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    } else {
+      echo "Password and password confirmation does not match!";
+    }
   }
 
   $user->save();
