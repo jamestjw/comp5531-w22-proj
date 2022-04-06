a9871c9fa3b787bc047501515720d0c5caaf54b6
Zachary_Concordia <zachary.jones@mail.concordia.ca>
2022-04-05 20:02:43 -0400

    Initial Commit of first rename changes.

--- modules/models/lecture_instructor.php
+++ modules/models/lecture_instructor.php
@@ -1,23 +1,30 @@
 <?php
 
+<<<<<<< HEAD:modules/models/course_offering_instructor.php
 require_once(dirname(__FILE__)."/record.php");
 
 class CourseOfferingInstructor extends Record
 {
     protected static $table_name = "course_offerings_instructors";
 
+=======
+class LectureInstructor extends Record {
+
+    static protected $table_name = "lecture_instructors";
+    
+>>>>>>> 51c4ab4 (Initial Commit of first rename changes.):modules/models/lecture_instructor.php
     protected static $belongs_to = array(
         "user" => array(
             "class_name" => "User",
             "foreign_key" => "user_id",
         ),
-        "course_offering" => array(
-            "class_name" => "CourseOffering",
-            "foreign_key" => "offering_id",
+        "lecture" => array(
+            "class_name" => "Lecture",
+            "foreign_key" => "lecture_id",
         )
     );
 
-    public $offering_id;
+    public $lecture_id;
     public $user_id;
     public $created_at;
     public $updated_at;
