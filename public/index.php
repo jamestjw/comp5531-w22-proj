9e4f5db89421c3f8948a63ae30c13d35841ec211
drouba <andreanne.chartrand@gmail.com>
2022-04-21 13:38:31 -0400

    start index page styling and refactoring

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php require_once(dirname(__FILE__)."/../modules/ensure_logged_in.php"); ?>
+<link rel="stylesheet" href="css/index.css">
 <?php
 require_once "../common.php";
 
@@ -7,6 +8,8 @@ try {
 } catch (PDOException $error) {
     echo  $error->getMessage();
 }
+
+$user_role = get_current_role();
 ?>
 
 
@@ -15,15 +18,18 @@ try {
 <?php if (isset($latest_announcement)) {?>
   <h2>Latest Announcement: </h2>
   <h4> <?php echo $latest_announcement->announcement_text ?></h4>
-  <h6> Posted: <?php echo $latest_announcement->created_at ?> </h6>
+  <p> Posted: <?php echo $latest_announcement->created_at ?> </p>
 <?php } else {?>
 <h2>No announcement from admin</h2>
 <?php }?>
 
+<hr>
+
 
 <h2> Landing page </h2>
 
-<ul>
+<?php if($user_role == "admin") { ?>
+  <ul>
   <li>
     <a href="view_users.php"><strong>View</strong></a> - List all users
   </li>
@@ -32,5 +38,7 @@ try {
     <a href="course_list.php"><strong>View and Create</strong></a> - View and Add Courses
   </li>
 </ul>
+<?php } ?>
+
 
 <?php include "templates/footer.php"; ?>
